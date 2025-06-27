c694756299a076f708fec54e9c9bf7a7b7d109e4
Jkennedy2004 <jg374997@gmail.com>
2025-06-26 20:58:34 -0500

    Segundo Parcial 2

--- SengundoParcial/Practica-en-clases/practica-clases/src/repartidores/dto/create-repartidore.dto.ts
+++ SengundoParcial/Practica-en-clases/practica-clases/src/repartidores/dto/create-repartidore.dto.ts
@@ -1,8 +1,20 @@
+import { IsOptional, isString, isNumber, isNotEmpty, } from "class-validator";
+
 export class CreateRepartidoreDto {
+    @isNumber()
+    @IsOptional()
         id: number;
-        nombre: string;      
+    @isString()
+    @isNotEmpty()
+        nombre: string;
+    @isString()      
         apellido: string;
+    @isNumber()
         edad: number;
+    @isString()
+    @IsOptional
         nacionalidad: string;
+    @isString()
+    @IsOptional
         estado: string
 }
--- SengundoParcial/Practica-en-clases/practica-clases/src/repartidores/repartidores.service.ts
+++ SengundoParcial/Practica-en-clases/practica-clases/src/repartidores/repartidores.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRepartidoreDto } from './dto/create-repartidore.dto';
 import { UpdateRepartidoreDto } from './dto/update-repartidore.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repartidore } from './entities/repartidore.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RepartidoresService {
+  constructor(@InjectRepository(Repartidore)
+  private repository: Repository<Repartidore>,
+) {}
   create(createRepartidoreDto: CreateRepartidoreDto) {
+    const Repartidore = this.repository.create(createRepartidoreDto);
     return 'This action adds a new repartidore';
   }
 
